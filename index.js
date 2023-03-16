32f10369c17f66227705e9f9fce0ae79fda3c5b9
Giulia Hossu <102684067+giuli-hossu@users.noreply.github.com>
2023-03-16 13:30:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-const randomNumber1 = randomIntFromInterval(1, 6)
-const randomNumber2 = randomIntFromInterval(1, 6)
+document.querySelector('#button-1').addEventListener('click', function()
+{ 
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
-associateValueWithPic(randomNumber1,0)
-associateValueWithPic(randomNumber2,1)
-
-//If player 1 wins
-if (randomNumber1 > randomNumber2) 
-{
+  var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+  
+  var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+  
+  var image1 = document.querySelectorAll("img")[0];
+  
+  image1.setAttribute("src", randomImageSource);
+  
+  
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  
+  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+  
+  document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+  
+  
+  //If player 1 wins
+  if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
-}
-else if (randomNumber2 > randomNumber1) 
-{
+  }
+  else if (randomNumber2 > randomNumber1) {
     document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-} 
-else
-{
+  }
+  else {
     document.querySelector("h1").innerHTML = "Draw!";
-}
-
-function randomIntFromInterval(min, max) 
-{ // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
-function associateValueWithPic(randomNumber1,val)
-{
-    var randomImageSource = "images/dice" + randomNumber1 + ".png";; //dice1.png - dice6.png
-    document.querySelectorAll("img")[val].setAttribute("src", randomImageSource);
-}
+  }
+});
 
